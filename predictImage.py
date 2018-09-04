3675bc21f284216128f0b7e917085daafa105946
chad <chad@daotech.io>
2018-09-04 16:25:03 +0800

    change predictImage

--- predictImage.py
+++ predictImage.py
@@ -4,17 +4,20 @@
 from django.http import HttpResponse
 from predict import *
 import json
+import time
+import os
+import re
+import base64
+from cStringIO import StringIO
+
+from PIL import Image
 
 def recognition(request):
     if request.method == 'POST':
-        file_obj = request.FILES.get('image')
-        import os
-        imagePath = os.path.join('static', 'pic', file_obj.name)
-        f = open(imagePath, 'wb')
-        print(file_obj,type(file_obj))
-        for chunk in file_obj.chunks():
-            f.write(chunk)
-        f.close()
+        base64Str = request.POST['image']
+        nowStr = time.strftime("%Y_%m_%d_%H_%M_%S",time.localtime(time.time()))
+        imagePath = os.path.join('static', 'pic', nowStr + r".jpeg")
+        base64_to_image(base64Str, imagePath)
         imageType = predictImage(imagePath)
         os.remove(imagePath)
     if imageType == 0:
@@ -27,3 +30,12 @@ def recognition(request):
             'data': data,
         }
     return HttpResponse(json.dumps(resp, {'ensure_ascii':False}), content_type="application/json")
+
+def base64_to_image(base64_str, image_path=None):
+    base64_data = re.sub('^data:image/.+;base64,', '', base64_str)
+    binary_data = base64.b64decode(base64_data)
+    img_data = StringIO(binary_data)
+    img = Image.open(img_data)
+    if image_path:
+        img.save(image_path)
+    return img
